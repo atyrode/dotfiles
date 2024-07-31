12974eefd4c4a3e818ef8fdc29db11f4875ca147
Alex TYRODE <alex.tyrode@outlook.fr>
2024-07-31 03:34:23 +0200

    update install and readme

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Exit immediately if a command exits with a non-zero status
-set -e 
-
 # Install Nix
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
 

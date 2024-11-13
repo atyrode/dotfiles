277c65957f71900d14106967fde6ca99585ca8b7
Alex Tyrode <alex.tyrode@outlook.fr>
2024-11-13 03:23:53 -0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 # Install Nix
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
 
-nix --version
+# Start a new shell and check the Nix version in that shell
+exec $SHELL -c "nix --version"

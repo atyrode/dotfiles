1c9a029a3f0b85119670162aefab52a8b9aadafe
Alex <alex@tyrode.dev>
2026-06-14 20:23:36 +0200

    Gate fastfetch on shell startup
    Fixes #3

--- home/shell/startup.zsh
+++ home/shell/startup.zsh
@@ -2,4 +2,16 @@
 # Startup footer
 ############################################
 
-fastfetch
+_should_run_fastfetch() {
+  [[ -o interactive ]] || return 1
+  [[ "${NIX_DOTFILES_FASTFETCH:-1}" != "0" ]] || return 1
+  [[ -z "${NIX_DOTFILES_FASTFETCH_SHOWN:-}" ]] || return 1
+  [[ -z "${CI:-}" ]] || return 1
+  [[ -z "${SSH_CONNECTION:-}" ]] || return 1
+  [[ "${TERM_PROGRAM:-}" != "vscode" ]] || return 1
+}
+
+if _should_run_fastfetch; then
+  export NIX_DOTFILES_FASTFETCH_SHOWN=1
+  fastfetch
+fi
